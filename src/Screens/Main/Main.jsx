28fb32ba7fd9199faab5120e8d3036e970cb5dc0
gaurav <gauravsoni8414@gmail.com>
2023-06-03 14:18:45 +0530

    main page ui set

--- src/Screens/Main/Main.jsx
+++ src/Screens/Main/Main.jsx
@@ -19,8 +19,8 @@ const Main = () => {
           <hr class="h-px my-4 bg-gray-100 border-0 dark:bg-gray-700" />
           <BalanceStats />
           <hr class="h-px my-4 bg-gray-100 border-0 dark:bg-gray-800" />
-          <RedirectTabButtons text=" Use 80% Money for Needs & Wants "/>
-          <RedirectTabButtons text=" Use 20% Money for Investments "/>
+          <RedirectTabButtons text="Needs & Wants (70%)" />
+          <RedirectTabButtons text="Investments (30%)" />
         </>
       )}
     </div>

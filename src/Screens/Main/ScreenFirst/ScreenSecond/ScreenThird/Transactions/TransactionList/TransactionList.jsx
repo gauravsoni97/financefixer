035bfea8a0c292607bb2ea3af72d60f507e5d09c
gaurav <gauravsoni8414@gmail.com>
2023-06-03 22:49:47 +0530

    transaction ui set

--- src/Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/TransactionList/TransactionList.jsx
+++ src/Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/TransactionList/TransactionList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TransactionList = () => {
   return (
-    <div className="py-3 max-h-[80vh] overflow-auto">
+    <div className="py-3 max-h-[70vh] overflow-auto">
       <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
         <span>Item Name</span>
         <span>135432 </span>

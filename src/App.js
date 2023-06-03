e7ac7222a064309bfd3728f2b770551ddf94c40f
gaurav <gauravsoni8414@gmail.com>
2023-06-03 11:40:57 +0530

    login ui set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Login from "./Screens/Login/Login";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Login />
+    </div>
+  );
+};
 
-export default App
+export default App;

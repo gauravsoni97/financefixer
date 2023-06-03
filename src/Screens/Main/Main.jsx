af51c807a9ffd106584fb398c2dcbe1b8f7db9d7
gaurav <gauravsoni8414@gmail.com>
2023-06-03 12:24:49 +0530

    form added in main component

--- src/Screens/Main/Main.jsx
+++ src/Screens/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProfileHero from './ProfileHero/ProfileHero'
+import MonthlyInput from './MonthlyInput/MonthlyInput'
 
 const Main = () => {
   return (
-    <div className=' h-screen w-screen flex items-start p-3 justify-center  bg-gray-50 dark:bg-gray-900'>
+    <div className=' h-screen w-screen  p-3  bg-gray-50 dark:bg-gray-900'>
       <ProfileHero/>
+      <MonthlyInput/>
     </div>
   )
 }
--- src/Screens/Main/ProfileHero/ProfileHero.jsx
+++ src/Screens/Main/ProfileHero/ProfileHero.jsx
@@ -12,13 +12,16 @@ const ProfileHero = () => {
   return (
     <>
       <div className="w-full max-w-md relative bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="logoutButton absolute right-0 top-1.5">
+          <button
+            type="button"
+            class="py-2 px-3 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Logout
+          </button>
+        </div>
 
-<div className="logoutButton absolute right-0 top-1.5">
-<button type="button" class="py-2 px-3 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Logout</button>
-</div>
-
-       
-        <div className="flex flex-col items-center py-10">
+        <div className="flex flex-col items-center py-6 pb-7">
           <img
             className="w-24 h-24 mb-3 rounded-full shadow-lg"
             src={myImg}

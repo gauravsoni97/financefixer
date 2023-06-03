1380c3f64ff449a0c5ba368347a5d36e8a0ed4d3
gaurav <gauravsoni8414@gmail.com>
2023-06-03 12:09:25 +0530

    Add: Profile hero set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./Screens/Login/Login";
+import Main from "./Screens/Main/Main";
 
 const App = () => {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Main/>
     </div>
   );
 };
--- src/Screens/Main/Main.jsx
+++ src/Screens/Main/Main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ProfileHero from './ProfileHero/ProfileHero'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className=' h-screen w-screen flex items-start p-3 justify-center  bg-gray-50 dark:bg-gray-900'>
+      <ProfileHero/>
+    </div>
   )
 }
 

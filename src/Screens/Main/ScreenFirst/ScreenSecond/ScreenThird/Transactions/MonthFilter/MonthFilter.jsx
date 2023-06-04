4ea25c1f9fd687dfa8a85058b9065b647892f0d1
gaurav <gauravsoni8414@gmail.com>
2023-06-04 11:47:40 +0530

    transaction page set

--- src/Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/MonthFilter/MonthFilter.jsx
+++ src/Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/MonthFilter/MonthFilter.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const MonthFilter = () => {
   return (
-    <div className="flex item-center justify-between py-5">
-        <div className="flex item-center justify-center ">Select Month</div>
+    <div className="flex items-center justify-between py-5">
+        <div className="flex items-center justify-center">Select Month</div>
         <select
           id="countries"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[50%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-auto"
+          class="bg-gray-50 borderdark:bg-gray-800 dark:border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[20%] p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-auto dark:bg-gray-800 dark:border-gray-400"
         >
           <option selected>All</option>
           <option value="US">Jan</option>
--- src/Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/TransactionList/TransactionList.jsx
+++ src/Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/TransactionList/TransactionList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 
 const TransactionList = () => {
   return (
@@ -6,103 +7,11 @@ const TransactionList = () => {
       <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
         <span>Item Name</span>
         <span>135432 </span>
-        <span>X</span>
+        <span>
+          <DeleteForeverRoundedIcon/>
+        </span>
       </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
-      <div className="flex item-center justify-between border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-800 dark:border-gray-400  dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2">
-        <span>Item Name</span>
-        <span>135432 </span>
-        <span>X</span>
-      </div>
+     
     </div>
   );
 };

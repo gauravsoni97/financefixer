47897dceacb412892a30dea340522fbaf2690993
gaurav <gauravsoni8414@gmail.com>
2023-06-04 13:35:25 +0530

    react router added in app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Login from "./Screens/Login/Login";
 import Main from "./Screens/Main/Main";
 import CommonFormPage from "./Screens/Main/ScreenFirst/ScreenSecond/CommonFormPage/CommonFormPage";
 import Transactions from "./Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/Transactions";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-
   return (
-    <div className="max-w-md mx-auto " >
-      <Main/>
+    <div className="max-w-md mx-auto ">
+      <Routes>
+        <Route path="/" Component={Main} />
+        <Route path="/commonform" Component={CommonFormPage} />
+        <Route path="/transactions" Component={Transactions} />
+        <Route path="/login" Component={Login} />
+      </Routes>
     </div>
   );
 };

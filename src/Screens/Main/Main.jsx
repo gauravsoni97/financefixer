5e3e3561e7735079dff5eb7e4f03553cf8e2ad65
gaurav <gauravsoni8414@gmail.com>
2023-06-04 14:34:05 +0530

    main ui set

--- src/Screens/Main/Main.jsx
+++ src/Screens/Main/Main.jsx
@@ -4,6 +4,7 @@ import RedirectTabButtons from "../../Components/RedirectButtons/RedirectTabButt
 import MonthlyInput from "./ScreenFirst/MonthlyInput/MonthlyInput";
 import ProfileHero from "./ScreenFirst/ProfileHero/ProfileHero";
 import ResetAppModal from "./ScreenFirst/ResetAppModal/ResetAppModal";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   const [modal, setModal] = useState(false);
@@ -17,11 +18,23 @@ const Main = () => {
           <ProfileHero />
           <MonthlyInput setModal={setModal} />
           <BalanceStats />
-          <div className="flex flex-col">
 
-          <RedirectTabButtons text="Needs & Wants (70%)" />
-          <RedirectTabButtons text="Investments (30%)" />
-          </div>
+          <Link to="/commonformpage">
+            <button
+              type="button"
+              class="w-full text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-md  py-5 text-center mr-2 mb-4 max-w-md"
+            >
+              Needs + Wants (70%)
+            </button>
+          </Link>
+          <Link to="/commonformpage">
+            <button
+              type="button"
+              class="w-full text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-md  py-5 text-center mr-2 mb-4 max-w-md"
+            >
+              Investments (30%)
+            </button>
+          </Link>
         </>
       )}
     </div>

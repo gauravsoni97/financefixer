7b50d34fea4fca0f01117cb4fc46cf718bd9d0e7
gaurav <gauravsoni8414@gmail.com>
2023-06-05 21:06:15 +0530

    deleted whole code and restarting again

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React from "react";
-import Login from "./Screens/Login/Login";
-import Main from "./Screens/Main/Main";
-import CommonFormPage from "./Screens/Main/ScreenFirst/ScreenSecond/CommonFormPage/CommonFormPage";
-import Transactions from "./Screens/Main/ScreenFirst/ScreenSecond/ScreenThird/Transactions/Transactions";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
 
 const App = () => {
   return (
     <div className="max-w-md mx-auto ">
-      <Routes>
-        <Route path="/" Component={Main} />
-        <Route path="/commonformpage" Component={CommonFormPage} />
-        <Route path="/transactions" Component={Transactions} />
-        <Route path="/login" Component={Login} />
-      </Routes>
+    
     </div>
   );
 };

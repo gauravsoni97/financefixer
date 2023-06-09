9979db85a56d94aa7e5a0dcd602cb4d2ac51cac5
gaurav <gauravsoni8414@gmail.com>
2023-06-09 21:31:15 +0530

    main component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import Login from "./Screens/Login/Login";
+import { Route, Routes } from "react-router-dom";
+import Main from "./Screens/Main/Main";
 
 const App = () => {
   return (
     <div className="max-w-md mx-auto ">
-    <Login/>
+      <Login />
     </div>
   );
 };
--- src/Screens/Login/Login.jsx
+++ src/Screens/Login/Login.jsx
@@ -6,6 +6,8 @@ import facebookImg from "../../assets/Images/facebook.png";
 import { auth, provider } from "../../Utils/firebase";
 
 import { signInWithPopup } from "firebase/auth";
+import { Link } from "react-router-dom";
+import Main from "../Main/Main";
 
 const Login = () => {
   const [value, setValue] = useState("");
@@ -26,25 +28,25 @@ const Login = () => {
   return (
     <div>
       {value ? (
-        "i am logged in "
+      <Main/>
       ) : (
         <section className=" h-screen max-w-md w-screen flex items-center justify-center  bg-gray-50 dark:bg-gray-900">
           <div className="flex w-screen flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a
-              href="#"
+            <Link
+              to="/"
               className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
             >
               <img className="w-7 h-7 mr-2" src={logo} alt="logo" />
               Finance Fixer
-            </a>
+            </Link>
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <div>
                   <ul className="my-4 space-y-3">
                     <li>
-                      <a
+                      <Link
                         onClick={handleGoogleClick}
-                        href="#"
+                        to="/login"
                         className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
                       >
                         <img
@@ -55,7 +57,7 @@ const Login = () => {
                         <span className="flex-1 ml-3 whitespace-nowrap">
                           Sign in with Google
                         </span>
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </div>

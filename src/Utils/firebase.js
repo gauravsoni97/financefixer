f94ec86dfdd871e6102ce55a63ec4621d5e5a305
gaurav <gauravsoni8414@gmail.com>
2023-06-10 21:26:59 +0530

    firebase api key safe set in env file

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD3J_zhedjrAHxcrylnIKGzrQu1zRn5KzU",
-  authDomain: "financefixerauth.firebaseapp.com",
-  projectId: "financefixerauth",
-  storageBucket: "financefixerauth.appspot.com",
-  messagingSenderId: "1030013133298",
-  appId: "1:1030013133298:web:6ffae919571b0d098ecb6b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase

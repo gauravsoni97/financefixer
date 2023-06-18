b785f3f54f72b553da3c1d68b38c811f061a8228
gaurav <gauravsoni8414@gmail.com>
2023-06-18 18:19:30 +0530

    sign in with redirect added

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -14,7 +14,7 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider);
+    signInWithRedirect(auth, provider);
   };
 
   const logOut = () => {

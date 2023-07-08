95323fddfdee2cdcb46a1343abab17350777cd1a
gaurav <gauravsoni8414@gmail.com>
2023-07-08 08:35:21 +0530

    slider range set full set component

--- src/Screens/Main/MainPage/RangeSlider/RangeSlider.jsx
+++ src/Screens/Main/MainPage/RangeSlider/RangeSlider.jsx
@@ -18,19 +18,26 @@ const RangeSlider = () => {
   console.log(sliderValue);
 
   return (
-    <div className="flex items-center justify-between">
-      <input
-        style={{ width: "85%" }}
-        type="range"
-        min={10}
-        max={100}
-        step={5}
-        defaultValue={70}
-        value={rangeInputValue}
-        onChange={handleSliderValue}
-      />
-      <div>{sliderValue?sliderValue:rangeInputValue}%</div>
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <span className="text-sm">Needs + Wants</span>
+        <span className="text-sm">Investments</span>
+      </div>
+      <div className="flex items-center justify-between gap-2">
+        <div>{sliderValue ? sliderValue : rangeInputValue}%</div>
+        <input
+          style={{ width: "85%" }}
+          type="range"
+          min={10}
+          max={100}
+          step={5}
+          defaultValue={70}
+          value={rangeInputValue}
+          onChange={handleSliderValue}
+        />
+        <div>{sliderValue ? 100 - sliderValue : 100 - rangeInputValue}%</div>
+      </div>
+    </>
   );
 };
 

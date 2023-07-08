8af7e136a2159f8d3937a6456bff2e09ac43ff6e
gaurav <gauravsoni8414@gmail.com>
2023-07-08 10:50:33 +0530

    action buttons rates set

--- src/Screens/Main/MainPage/RedirectToPage.jsx
+++ src/Screens/Main/MainPage/RedirectToPage.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { useSliderRangeContext } from "../../../context/SliderRangeValue";
 
 const RedirectToPage = ({ setNeedsWantsScreen, setInvestmentsScreen }) => {
+  const contextValue = useSliderRangeContext();
+  const { sliderValue } = contextValue || {};
+
+
   return (
     <div className="w-full max-w-sm p-4 py-6 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 shadow dark:bg-gray-800 dark:border-gray-700 mb-3">
       <button onClick={()=>setNeedsWantsScreen()}
         type="button"
         className="py-4 px-5  mb-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 w-full"
       >
-        Needs + Wants (70%)
+        Needs & Wants ({sliderValue}%)
       </button>
       <button onClick={()=>setInvestmentsScreen()}
         type="button"
         className="py-4 px-5  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 w-full"
       >
-        Investments (30%)
+        Investments ({100 - sliderValue}%)
       </button>
     </div>
   );
--- src/Screens/Main/MainPage/TotalBalance.jsx
+++ src/Screens/Main/MainPage/TotalBalance.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const TotalBalance = ({totalAvailableBalance,
   totalUsedBalance,}) => {
+   
   return (
     <div className="w-full max-w-sm p-8 px-6 bg-white border border-gray-200 rounded-lg shadow shadow dark:bg-gray-800 dark:border-gray-700 mb-3">
     <div className="flex justify-between mb-3">
